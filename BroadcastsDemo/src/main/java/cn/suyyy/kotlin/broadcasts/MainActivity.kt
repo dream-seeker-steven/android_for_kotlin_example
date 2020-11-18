b6ebc64c47d957af4464a78bf142237a08dd3f40
dream-seeker-steven <dreamseeker-steven@qq.com>
2020-11-18 19:52:12 +0800

    init

---
+++ BroadcastsDemo/src/main/java/cn/suyyy/kotlin/broadcasts/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.suyyy.kotlin.broadcasts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.suyyy.kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
