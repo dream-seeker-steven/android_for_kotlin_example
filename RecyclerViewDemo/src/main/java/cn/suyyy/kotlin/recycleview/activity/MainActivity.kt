1f61aeda062317e289d93ad658f491ec1183409a
dream-seeker-steven <dreamseeker-steven@qq.com>
2020-11-18 20:25:05 +0800

    RecycleView 瀑布流

--- RecyclerViewDemo/src/main/java/cn/suyyy/kotlin/recycleview/activity/MainActivity.kt
+++ RecyclerViewDemo/src/main/java/cn/suyyy/kotlin/recycleview/activity/MainActivity.kt
@@ -3,12 +3,14 @@ package cn.suyyy.kotlin.recycleview.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import cn.suyyy.kotlin.R
 import cn.suyyy.kotlin.recycleview.adaper.FruitAdapter
 import cn.suyyy.kotlin.recycleview.data.Fruit
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     private val fruitList = ArrayList<Fruit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +20,8 @@ class MainActivity : AppCompatActivity() {
         // 初始化数据
         initFruit()
         // 创建布局管理器
-        val layoutManager = LinearLayoutManager(this)
-        // 水平布局
-        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        val layoutManager =
+            StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.layoutManager = layoutManager
         // 装载适配器
         val adapter = FruitAdapter(fruitList)
@@ -28,19 +29,28 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
     }
 
-    private fun initFruit(){
+    private fun initFruit() {
         // 重复10次
-        repeat(10){
-            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
-            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
-            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
-            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
-            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
-            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
-            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
-            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
-            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
-            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        repeat(10) {
+            fruitList.add(Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic))
+            fruitList.add(Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic))
+            fruitList.add(Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic))
+            fruitList.add(Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic))
+            fruitList.add(Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic))
+            fruitList.add(Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic))
+            fruitList.add(Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic))
+            fruitList.add(Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic))
+            fruitList.add(Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic))
+            fruitList.add(Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic))
         }
+    }
+
+    private fun getRandomLengthName(str: String): String {
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n){
+            builder.append(str)
+        }
+        return builder.toString()
     }
 }
