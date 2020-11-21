6852e5b39a664934b93d76b3d43e22224cb954a7
dream-seeker-steven <dreamseeker-steven@qq.com>
2020-11-21 12:38:43 +0800

    Notification 通知

--- NotificationDemo/src/main/java/cn/suyyy/notificationdemo/MainActivity.kt
+++ NotificationDemo/src/main/java/cn/suyyy/notificationdemo/MainActivity.kt
@@ -1,11 +1,56 @@
 package cn.suyyy.notificationdemo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * 1.获取NotificationManager
+ * 2.判断安卓版本创建通知渠道
+ * 3.创建通知
+ * 4.使用PendingIntent为通知设置点击事件
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // O 不是 0 是代表 api 26 即奥利奥 安卓8.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel =
+                NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+
+            // 更重要的通知
+            val channel2 = NotificationChannel("important", "Important", NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel2)
+        }
+        sendNotice.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            val pi = PendingIntent.getActivity(this, 0, intent, 0)
+            // val notification = NotificationCompat.Builder(this, "normal")
+            val notification = NotificationCompat.Builder(this, "important")
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setSmallIcon(R.drawable.small_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                //.setStyle(NotificationCompat.BigTextStyle().bigText("Learn how to build notifications, send and sync data, and use voice actions. Get the official Android IDE and developer tools to build apps for Android."))
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(resources, R.drawable.big_image)))
+                .build()
+            // 要保证每个通知指定的 id 都是不同的
+            manager.notify(1,notification)
+        }
     }
 }
---
+++ NotificationDemo/src/main/java/cn/suyyy/notificationdemo/NotificationActivity.kt
@@ -0,0 +1,13 @@
+package cn.suyyy.notificationdemo
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
